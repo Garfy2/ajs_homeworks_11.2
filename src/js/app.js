76419ddf3e2b5b74f248a38812a632c8bdb6b711
Garfy2 <105547060+Garfy2@users.noreply.github.com>
2023-03-12 22:24:11 +0300

    upload

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+// import Character from './characters/character';'./characters/character';
+// import Bowerman from './characters/bowerman';
+// import Swordsman from './characters/swordsman';
+// import Magician from './characters/magician';
+// import Daemon from './characters/daemon';
+// import Undead from './characters/undead';
+// import Zombie from './characters/zombie';
+// import Team from './team';
+
+// const player1 = new Bowerman('player1');
+// const player2 = new Swordsman('player2');
+// const player3 = new Magician('player3');
+// const player4 = new Daemon('player4');
+// const player5 = new Undead('player5');
+// // console.log(player1)
+// // console.log(player2)
+// // console.log(player3)
+// // console.log(player4)
+// // console.log(player5)
+
+// const team1 = new Team();
+// team1.addAll(player1, player2, player3, player4, player5);
+
+// for (let num of team1) {
+//   console.log(num);
+// }
